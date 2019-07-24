e4ef76b7d39dd47026b0794a167f98ace468260e
marcelgaedke <marcelgaedke@yahoo.com>
2019-07-24 21:26:25 +0200

    frth commit

--- gaedke/settings_private.py
+++ gaedke/settings_private.py
@@ -1,5 +1,5 @@
 
-IS_SERVER = False
+IS_SERVER = True
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -7,7 +7,8 @@ SECRET_KEY = '@f_^mw+=l#hb)5_^1j^sch))&a6_5-t*hh@+71ly-8&m3q2t2f'
 
 
 #STATIC_ROOT ="/root/PycharmProjects/gaedkegrabmale/gaedke/staticfiles"
-STATIC_ROOT = "/home/kidneybean/grabmalehamburg/website/grabmalehamburg_14_1_2019/gaedkenaturstein/htdocs/App1/static"
+#STATIC_ROOT = "/home/kidneybean/grabmalehamburg/website/grabmalehamburg_14_1_2019/gaedkenaturstein/htdocs/App1/static"
+
 
 #MEDIA_ROOT ="/home/kidneybean/Downloads/grabmalehamburg_3_10_18/htdocs/App1/static/"
 MEDIA_ROOT = "/root/PycharmProjects/gaedkegrabmale/gaedke/staticfiles"
